9ae0a78dd95e47daf30308c82dcf61f7e8867ade
Olava Faksdal <stiolafak@gmail.com>
2021-09-28 10:49:05 +0200

    added the honey badger

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,9 +1,41 @@
+import { Card, CardContent, CardActionArea, CardMedia, CardActions, Button, Typography } from '@mui/material';
 import React from 'react';
+import honeybadger from './../honey_badger.jpeg';
+
 
 class Content extends React.Component {
+
     render() {
         return(
-            <p>Hello</p>
+            <>
+            <div>
+                <Card sx={{ maxWidth: 345 }}>
+                    <CardActionArea>
+                        <CardMedia
+                        component="img"
+                        height="140"
+                        image={honeybadger}
+                        alt="honey badger"
+                        />
+                        <CardContent>
+                            <Typography gutterBottom variant="h5" component="div">
+                                Honey badger
+                            </Typography>
+                            <Typography variant="body2" color="text.secondary">
+                               It is the only living species in the genus Mellivora and in the mustelid subfamily Mellivorinae. 
+                               Despite its name, the honey badger does not closely resemble other badger species; instead, 
+                               it bears more anatomical similarities to weasels.
+                            </Typography>
+                        </CardContent>
+                    </CardActionArea>
+                    <CardActions>
+                        <Button size="small" color="primary">
+                            Share
+                        </Button>
+                    </CardActions>
+                </Card>
+            </div>
+            </>
         );
     }    
 }
