558c0a9a13a0d945cb3fff275f50fb7bd5b94f49
Olava Faksdal <stiolafak@gmail.com>
2021-09-28 09:56:32 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Content from './components/Content';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>How to React</h1>
+      </header>
+      <Content />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Content.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class Content extends React.Component {
+    render() {
+        return(
+            <p>Hello</p>
+        );
+    }    
+}
+export default Content;
